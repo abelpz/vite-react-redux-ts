b3e9dbdbaaac710bc3dcf6fa67dd0810332c4a13
Abel Pérez <abelper54@gmail.com>
2022-08-16 18:40:27 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+function App() {
+  return <Hello></Hello>;
+}
+
+export default App;
